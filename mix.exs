915327e2f3421235969e6c9fcaaa4476bb4cd164
Edgar Pino <me@edgarpino.com>
2017-08-26 08:24:34 -0600

    Added facebook dep

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule FbRanker.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:facebook, "~> 0.13.2"}
     ]
   end
 
