debe73d979eeb7775daaf6b6367837686cd45a2d
Edgar Pino <me@edgarpino.com>
2017-09-29 11:12:06 -0600

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :fb_ranker, FbRankerWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "https://immense-hamlet-15325.herokuapp.com/", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,6 +60,13 @@ config :logger, level: :info
 #     config :fb_ranker, FbRankerWeb.Endpoint, server: true
 #
 
+# Configure your database
+config :fb_ranker, FbRanker.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       url: System.get_env("DATABASE_URL"),
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: true
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
